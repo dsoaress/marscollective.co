07cd4355e5efe9a31ceab6483d44717dc7345dc2
Daniel Soares <daniel.soares@me.com>
2021-05-03 20:18:15 -0300

    chore: remove scripts

--- scripts/bump.sh
+++
@@ -1,9 +0,0 @@
-base_dir=$PWD
-
-cd ../../marscollective/marscollective.co/
-yarn lerna version
-
-cd $base_dir
-git fetch upstream
-git rebase upstream/main
-git push
--- scripts/rebase.sh
+++
@@ -1,4 +0,0 @@
-cd ../../marscollective/marscollective.co/
-git fetch upstream
-git rebase upstream/main
-git push
