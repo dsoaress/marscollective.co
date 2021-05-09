588eda572a38927d1750d42eccfa815730868a72
Daniel Soares <daniel.soares@me.com>
2021-05-08 22:29:45 -0300

    feat: add plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 import { ThemeProvider } from 'next-themes'
-import * as Fathom from 'fathom-client'
+import PlausibleProvider from 'next-plausible'
 
 import SEO from '@/components/SEO'
 import settings from '@/settings'
@@ -10,31 +8,16 @@ import 'typeface-rubik'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  const { events } = useRouter()
-
-  useEffect(() => {
-    window.dataLayer = window.dataLayer || []
-
-    Fathom.load(settings.fathom_id, {
-      url: `https://${settings.fathom_url}/script.js`,
-      includedDomains: [settings.site_url]
-    })
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview()
-    }
-
-    events.on('routeChangeComplete', onRouteChangeComplete)
-
-    return () => {
-      events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [])
-
   return (
     <ThemeProvider defaultTheme="system">
-      <SEO />
-      <Component {...pageProps} />
+      <PlausibleProvider
+        domain={settings.site_url}
+        customDomain={`https://stats.${settings.site_url}`}
+        trackOutboundLinks={true}
+      >
+        <SEO />
+        <Component {...pageProps} />
+      </PlausibleProvider>
     </ThemeProvider>
   )
 }
